66d8a2690a215fea00185592ec908184a565778c
llylee994-ui <liliya_0909@qq.com>
2025-09-25 20:21:43 +0800

    初始提交：添加Qt项目文件

---
+++ Test2/Test2/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Test2_FILE D:/qtproject/project2/samp2_4App/Test2/Test2/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/Test2.exe)
+set(__QT_DEPLOY_TARGET_Test2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Test2_RUNTIME_DLLS D:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
